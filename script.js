23d5cd406e13b1d272655192d3b00c3df1b4d283
Yeganeh Mohammadi <88578478+yeganeh98@users.noreply.github.com>
2024-04-27 15:05:48 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+$(document).ready(function() {
+    // Show the first section by default
+    $('#home').show();
+
+    // Navigation click actions
+    $('.main-menu a').on('click', function() {
+        var target = $(this).attr('href');
+        $('.content-region').hide();
+        $(target).show();
+        $('.main-menu a').removeClass('active');
+        $(this).addClass('active');
+    });
+});
 
